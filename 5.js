ee91fcd70677bc5a9e39d9fe43274b48df7ed8fd
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-04-04 17:01:30 +0530

    declared a variable using let and logges out its value bvefore updating

--- 5.js
+++ 5.js
@@ -1,6 +1,6 @@
 // Track the value of a variable by logging it before and after updating.
 
-//IN CASE OF VAR//
+//IN CASE OF VAR-----------------------//
 
 // var DOB = 28;
 // //before update//
@@ -8,3 +8,10 @@
 
 // DOB = 11;
 // console.log(DOB);
+
+//In case of let----------------------//
+
+//BEFORE UPDATING//
+
+let DOB = 28;
+console.log(DOB);
