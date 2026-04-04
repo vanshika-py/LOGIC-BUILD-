048dea2d32b6a1a72c8a7717e8e8adc63a3810d7
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-04-04 16:38:17 +0530

    solved by first method

--- 1.js
+++ 1.js
@@ -1,8 +1,14 @@
 // Perform and log the result of 45 * 2 – 10..........
 
 
-let a = 45;
-let b = 2;
-let c = 10;
+// let a = 45;
+// let b = 2;
+// let c = 10;
 
-console.log(a*b-c);
+// console.log(a*b-c);
+
+
+
+
+
+
---
+++ 4.js
@@ -0,0 +1,7 @@
+// Create two variables for first and last name. Concatenate and log them.
+
+let First_Name = "Vanshika";
+let Last_Name = "Kathpal";
+
+
+console.log(First_Name + " " + Last_Name);
