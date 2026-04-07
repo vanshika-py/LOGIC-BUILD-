90a4a5c0f46ee977feed69b0c9e0cfaff3df2a5f
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-04-08 00:22:48 +0530

    solved

--- 10.js
+++ 10.js
@@ -1,7 +1,7 @@
 // Create a for…of loop to log each character of the string "JavaScript".
 
-let language = "JavaScript";
+// let language = "JavaScript";
 
-for(let letter of language){
-    console.log(letter)
-}
+// for(let letter of language){
+//     console.log(letter)
+// }
--- 11.js
+++ 11.js
@@ -1,1 +1,6 @@
 // Write a for loop that skips even numbers between 1 and 20.
+
+for(let i= 1; i<=20; i++){
+   if(i%2 === 0) continue
+   console.log(i)
+}
