d300d18b65158ff25417c3c90a74aff84bf6ad65
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-04-12 00:32:49 +0530

    solved

--- 12.js
+++ 12.js
@@ -7,9 +7,9 @@
 // }
 
 //using do while loop
-let i = 5;
-do{
-    console.log(i);
-    i--;
-}
-    while(i >= 1);
+// let i = 5;
+// do{
+//     console.log(i);
+//     i--;
+// }
+//     while(i >= 1);
--- 13.js
+++ 13.js
@@ -1,1 +1,8 @@
 // .Create a for loop that calculates the factorial of 5.
+
+let n = 1;
+for(let i = 5; i>= 1; i--){
+    n*= i;
+}
+
+console.log(n);
