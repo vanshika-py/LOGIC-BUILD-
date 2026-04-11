de8aa87d6ee2ea713e8605b5c12be266af5eef12
Vanshika Kathpal <vanshikakathpal381@gmail.com>
2026-04-11 09:02:08 +0530

    solved using do whikle loop

--- 12.js
+++ 12.js
@@ -1,5 +1,15 @@
 // Use a do…while loop to log numbers from 5 to 1.
 
-for (i = 5; i >= 1; i--){
-    console.log(i)
+//using for loop 
+
+// for (i = 5; i >= 1; i--){
+//     console.log(i)
+// }
+
+//using do while loop
+let i = 5;
+do{
+    console.log(i);
+    i--;
 }
+    while(i >= 1);
